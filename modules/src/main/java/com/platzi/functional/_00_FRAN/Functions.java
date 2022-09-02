716e7b003e3514fad1640f8ca8200131666b96c6
Fran Manez <francisco.manez@upc.edu>
2022-09-02 12:09:43 +0200

    function

--- modules/src/main/java/com/platzi/functional/_00_FRAN/Functions.java
+++ modules/src/main/java/com/platzi/functional/_00_FRAN/Functions.java
@@ -1,4 +1,20 @@
 package com.platzi.functional._00_FRAN;
 
+import java.util.function.Function;
+
 public class Functions {
+    public static void main(String[] args) {
+        Function<Integer, Integer> fsquare = new Function<Integer, Integer>() {
+            @Override
+            public Integer apply(Integer x) {
+                return x * x;
+            }
+        };
+
+        System.out.println(fsquare.apply(4));
+
+        Function<Integer, Integer> fsquare2 = x -> x * x;
+        System.out.println(fsquare2.apply(4));
+
+    }
 }
